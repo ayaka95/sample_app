0d1b658a150c66933f308cd50d9b4251e484ebcf
EC2 Default User <ec2-user@ip-172-31-21-200.eu-north-1.compute.internal>
2024-12-18 06:51:41 +0000

    lists_spec.rb

---
+++ spec/models/list_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+describe "モデルに関するテスト", type: :model do
+  describe '実際に保存してみる' do
+    it "有効な投稿内容の場合は保存されるか" do
+      expect(FactoryBot.build(:list)).to be_valid
+    end
+  end
